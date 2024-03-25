ab8bdc0c474fddcda39f179f17e9db095f0c5199
Istrate Gheorghe-Iustin <justinistrate95@icloud.com>
2024-03-25 17:32:28 +0200

    import export udated

--- src/functions.php
+++ src/functions.php
@@ -1,32 +1,42 @@
 <?php
 require_once "databaseconn.php";
- if(isset($_POST["Import"])){
-    
-    $filename=$_FILES["file"]["tmp_name"];    
-     if($_FILES["file"]["size"] > 0)
-     {
-        $conn = getdb();
-        $file = fopen($filename, "r");
-          while (($getData = fgetcsv($file, 10000, ";")) !== FALSE)
-           {
-             $sql = "INSERT into autoparkdatabase (JUDET,CATEGORIE_NATIONALA,CATEGORIE_COMUNITARA,MARCA,DESCRIERE_COMERCIALA,TOTAL_VEHICULE) 
-                   values ('".$getData[0]."','".$getData[1]."','".$getData[2]."','".$getData[3]."','".$getData[4]."','".$getData[5]."')";
-                   $result = mysqli_query($conn, $sql);
-        if(!isset($result))
-        {
-          echo "<script type=\"text/javascript\">
+$conn = getdb();
+if (isset ($_POST["Import"])) {
+
+  $filename = $_FILES["file"]["tmp_name"];
+  if ($_FILES["file"]["size"] > 0) {
+    $file = fopen($filename, "r");
+    fgetcsv($file, 10000, ";");
+    while (($getData = fgetcsv($file, 10000, ";")) !== FALSE) {
+      $sql = "INSERT into autoparkdatabase (JUDET,CATEGORIE_NATIONALA,CATEGORIE_COMUNITARA,MARCA,DESCRIERE_COMERCIALA,TOTAL_VEHICULE) 
+                   values ('" . $getData[0] . "','" . $getData[1] . "','" . $getData[2] . "','" . $getData[3] . "','" . $getData[4] . "','" . $getData[5] . "')";
+      $result = mysqli_query($conn, $sql);
+      if (!isset ($result)) {
+        echo "<script type=\"text/javascript\">
               alert(\"Invalid File:Please Upload CSV File.\");
               window.location = \"index.php\"
-              </script>";    
-        }
-        else {
-            echo "<script type=\"text/javascript\">
+              </script>";
+      } else {
+        echo "<script type=\"text/javascript\">
             alert(\"CSV File has been successfully Imported.\");
             window.location = \"index.php\"
           </script>";
-        }
-           }
-           fclose($file);  
-     }
-  }   
- ?>
+      }
+    }
+    fclose($file);
+  }
+}
+if (isset ($_POST["Export"])) {
+
+  header('Content-Type: text/csv; charset=utf-8');
+  header('Content-Disposition: attachment; filename=data.csv');
+  $output = fopen("php://output", "w");
+  fputcsv($output, array('JUDET', 'CATEGORIE_NATIONALA', 'CATEGORIE_COMUNITARA', 'MARCA', 'DESCRIERE_COMERCIALA', 'TOTAL_VEHICULE'));
+  $query = "SELECT * from autoparkdatabase ORDER BY JUDET ASC";
+  $result = mysqli_query($conn, $query);
+  while ($row = mysqli_fetch_assoc($result)) {
+    fputcsv($output, $row,";");
+  }
+  fclose($output);
+}
+?>
--- src/index.php
+++ src/index.php
@@ -1,18 +1,26 @@
+<?php
+ini_set('max_execution_time', 60);
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+        crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
     <div id="wrap">
         <div class="container">
             <div class="row">
-                <form class="form-horizontal" action="functions.php" method="post" name="upload_excel" enctype="multipart/form-data">
+                <form class="form-horizontal" action="functions.php" method="post" name="upload_excel"
+                    enctype="multipart/form-data">
                     <fieldset>
                         <!-- Form Name -->
-                        <legend>Form Name</legend>
+                        <legend>Auto Park Smart Explorer</legend>
                         <!-- File Button -->
                         <div class="form-group">
                             <label class="col-md-4 control-label" for="filebutton">Select File</label>
@@ -24,17 +32,28 @@
                         <div class="form-group">
                             <label class="col-md-4 control-label" for="singlebutton">Import data</label>
                             <div class="col-md-4">
-                                <button type="submit" id="submit" name="Import" class="btn btn-primary button-loading" data-loading-text="Loading...">Import</button>
+                                <button type="submit" id="submit" name="Import" class="btn btn-primary button-loading"
+                                    data-loading-text="Loading...">Import</button>
                             </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="singlebutton">Export data</label>
+                            <form class="form-horizontal" action="functions.php" method="post" name="upload_excel"
+                                enctype="multipart/form-data">
+                                <div class="col-md-4 ">
+                                    <input type="submit" name="Export" class="btn btn-success" value="Export" />
+                                </div>
+                            </form>
                         </div>
                     </fieldset>
                 </form>
             </div>
             <?php
             require_once "getrecords.php";
-               get_all_records();
+            get_all_records();
             ?>
         </div>
     </div>
 </body>
+
 </html>
