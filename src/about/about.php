a3f6ee58b51258c4dc620b6dd09ffe769b9323cd
seerbann <serbypwp@yahoo.com>
2024-04-12 18:01:51 +0300

    about page text updated

--- src/about/about.php
+++ src/about/about.php
@@ -5,10 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IEEE System Requirements Specification</title>
     <link rel="stylesheet" href="styles.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-        crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
-        crossorigin="anonymous">
+
 </head>
 <body>
     <div class="navBar">
@@ -16,54 +13,84 @@
     </div>
     <div class="container">
         <header>
-            <h1>IEEE System Requirements Specification</h1>
+            <h1>AutoPark Smart Explorer Documentation</h1>
         </header>
 
         <nav>
             <ul>
                 <li><a href="#introduction">1.Introduction</a></li>
                 <li><a href="#overall-description">2.Overall Description</a></li>
-                <li><a href="#external-interface-requirements">3.External Interface Requirements</a></li>
-                <li><a href="#system-features">4.System Features</a></li>
-                <li><a href="#other-nonfunctional-requirements">5.Other Nonfunctional Requirements</a></li>
+                <li><a href="#pages">3.Pages</a></li>
+                <li><a href="#technologies-stack">4.Technologies Stack</a></li>
+                <li><a href="#functional-requirements">5.Functional requirements</a></li>
                 <li><a href="#other-requirements">6.Other Requirements</a></li>
-                <li><a href="#appendices">7.Appendices</a></li>
+                <li><a href="#bonuses">7.Bonuses</a></li>
             </ul>
         </nav>
 
         <section id="introduction">
             <h2>1. Introduction</h2>
-            
-            <h3>1.1 Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br/> 1.2 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h3>
-        </section>
+            <h4>1.1 The website aims to provide a platform for users to explore and interact with CSV files regarding automotive data, as well as view data and graphics related to these files. Additionally, an admin login feature will allow administrators to manage and import CSV documents.</h4>
+            </section>
 
         <section id="overall-description">
             <h2>2. Overall Description</h2>
            
         </section>
 
-        <section id="external-interface-requirements">
-            <h2>3. External Interface Requirements</h2>
-            
+        <section id="pages">
+            <h2>3. Pages</h2>
+                <h1>Website Pages</h1>
+
+    <!-- Home Page -->
+    <section>
+        <h3>Home Page</h3>
+        <p>Display information about the website's purpose and features.</p>
+    </section>
+
+    <!-- CSV Explorer Page -->
+    <section>
+        <h3>CSV Explorer Page</h3>
+        <p>Allows users to view a list of CSV documents.</p>
+        <p>Provides options to download or preview the contents of each CSV file.</p>
+    </section>
+
+    <!-- Data Explorer Page -->
+    <section>
+        <h3>Data Explorer Page</h3>
+        <p>Users can preview data from CSV files.</p>
+        <p>Includes graphical representations of the data, such as charts or graphs.</p>
+    </section>
         </section>
 
-        <section id="system-features">
-            <h2>4. System Features</h2>
-           
+        <section id="technologies-stack">
+            <h2>4. Technologies Stack</h2>
+           <section>
+        <h3>Front end</h3>
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+        </ul>
+        <h3>Back end</h3>
+        <ul>
+            <li>PHP</li>
+            <li>MySQL</li>
+    </section>
         </section>
 
-        <section id="other-nonfunctional-requirements">
-            <h2>5. Other Nonfunctional Requirements</h2>
-            
+        <section id="functional-requirements">
+            <h2>5. Functional requirements</h2>
+            <h3>SOON</h3>
         </section>
 
         <section id="other-requirements">
             <h2>6. Other Requirements</h2>
-            
+            <h3>Responsive design</h3>
         </section>
 
-        <section id="appendices">
-            <h2>Appendices</h2>
+        <section id="bonuses">
+            <h2>Bonuses</h2>
            
         </section>
     </div>
