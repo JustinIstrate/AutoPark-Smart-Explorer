05314df1951da6f1ec0125ec026bd164640f3915
Istrate Gheorghe-Iustin <justinistrate95@icloud.com>
2024-04-12 19:39:32 +0300

    dataExplorer updated

--- src/dataExplorer/index.php
+++ src/dataExplorer/index.php
@@ -1,6 +1,8 @@
 <?php
-ini_set('max_execution_time', 60);
+require_once "databaseconn.php"; // Include the file with your database connection function
+require_once "getrecords.php"; // Include the file with your get_all_records() function
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,7 +10,7 @@ ini_set('max_execution_time', 60);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AutoPark Smart Explorer</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="../dataExplorer/styles.css">
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,100;1,200;1,300;1,400&family=Tilt+Neon&display=swap"
         rel="stylesheet">
@@ -18,38 +20,19 @@ ini_set('max_execution_time', 60);
     <?php include '../common/navBar.php'; ?>
     <div id="wrap">
         <div class="container">
-            <form class="form-horizontal" action="functions.php" method="post" name="upload_excel"
-                enctype="multipart/form-data">
-                <fieldset>
-                    <div class="form-group">
-                        <label class="col-md-4 control-label" for="filebutton">Select File</label>
-                        <div class="col-md-4">
-                            <input type="file" name="file" id="file" class="input-large">
-                        </div>
-                    </div>
-                    <!-- Button -->
-                    <div class="form-group">
-                        <label class="col-md-4 control-label" for="singlebutton">Import data</label>
-                        <div class="col-md-4">
-                            <button type="submit" id="submit" name="Import" class="btn btn-primary button-loading"
-                                data-loading-text="Loading...">Import</button>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="col-md-4 control-label" for="singlebutton">Export data</label>
-                        <form class="form-horizontal" action="functions.php" method="post" name="upload_excel"
-                            enctype="multipart/form-data">
-                            <div class="col-md-4 ">
-                                <input type="submit" name="Export" class="btn btn-success" value="Export" />
-                            </div>
-                        </form>
-                    </div>
-                </fieldset>
-            </form>
-            <?php
-            require_once "getrecords.php";
-            get_all_records();
-            ?>
+            <div class="form-group">
+                <div class="col-md-4">
+                    <input type="file" name="file" id="file" class="input-large">
+                </div>
+                <div class="col-md-4">
+                    <button type="submit" id="submit" name="Import" class="btn btn-primary button-loading"
+                        data-loading-text="Loading...">Import</button>
+                </div>
+                <div class="col-md-4">
+                    <input type="submit" name="Export" class="btn btn-success" value="Export" />
+                </div>
+            </div>
+            <?php get_all_records(); ?>
         </div>
     </div>
 </body>
