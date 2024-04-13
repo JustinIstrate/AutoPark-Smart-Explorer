8c0472886b1a218579f48f526ebbd6cf69c9c46a
Istrate Gheorghe-Iustin <justinistrate95@icloud.com>
2024-04-13 17:08:30 +0300

    loginAdded

---
+++ src/login/auth.php
@@ -0,0 +1,22 @@
+<?php
+include ('connection.php');
+$username = $_POST['first'];
+$password = $_POST['password'];
+
+$username = mysqli_real_escape_string($con, $username);
+$password = mysqli_real_escape_string($con, $password);
+
+$sql = "SELECT * FROM form WHERE username = '$username' AND password = '$password'";
+$result = mysqli_query($con, $sql);
+$count = mysqli_num_rows($result);
+
+if ($count == 1) {
+    session_start();
+    $_SESSION['message'] = "Login successful!";
+    header("Location: ../dataExplorer/index.php");
+} else {
+    session_start();
+    $_SESSION['message'] = "Login failed. Invalid username or password.";
+    header("Location: form.php");
+}
+?>
